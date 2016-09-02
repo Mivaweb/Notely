bd0d7e09adc4c11597cb703dfcfa87ec7aae9de2
Michaël Vanbrabandt <info@mivaweb.be>
2016-09-02 13:17:30 +0200

    Add new tree controller for the notely section

---
+++ src/Notely/Notely.Web/Trees/NotelyTreeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Trees;
+
+namespace Notely.Web.Trees
+{
+    /// <summary>
+    /// Defines a new tree for the notely section
+    /// </summary>
+    [Tree("notely", "notely", "Notely")]
+    [PluginController("Notely")]
+    public class NotelyTreeController : TreeController
+    {
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+        {
+            var nodes = new TreeNodeCollection();
+
+            // ===> Routepath has to be: /appsection/treealias/htmlview/ID 
+            nodes.Add(this.CreateTreeNode("comments", id, queryStrings, "All comments", "icon-notepad", false, "/commentor/commentor/allcomments/manage"));
+            nodes.Add(this.CreateTreeNode("myComments", id, queryStrings, "My comments", "icon-operator", false, "/commentor/commentor/mycomments/manage"));
+            nodes.Add(this.CreateTreeNode("cleanup", id, queryStrings, "Cleanup", "icon-trash-alt-2", false, "/commentor/commentor/cleanup/manage"));
+
+            return nodes;
+        }
+
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+        {
+            return new MenuItemCollection();
+        }
+    }
+}
