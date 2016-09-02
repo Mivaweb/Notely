c29076fa21a6b4a0b38bae4dc19ee7e90e0b5074
Michaël Vanbrabandt <info@mivaweb.be>
2016-09-02 13:14:25 +0200

    Add new application section for notely

---
+++ src/Notely/Notely.Web/Trees/NotelyApp.cs
@@ -0,0 +1,16 @@
+﻿using umbraco.businesslogic;
+using umbraco.interfaces;
+using Umbraco.Web.Mvc;
+
+namespace Notely.Web.Trees
+{
+    /// <summary>
+    /// Defines a new application ( section ) for Notely
+    /// </summary>
+    [Application("Notely", "Notely", "icon-notepad", 10)]
+    [PluginController("Notely")]
+    public class NotelyApp : IApplication
+    {
+
+    }
+}
